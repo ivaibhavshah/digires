0997c41834397a3726163af68f62c828bee096d5
Vaibhav Abhaybhai Shah <vaibhav.anjar@gmail.com>
2022-08-17 12:38:06 +0530

    working js modal

--- resume_project/static/js/modal.js
+++ resume_project/static/js/modal.js
@@ -3,26 +3,24 @@ var loginmodal = document.querySelector(".loginmodal");
 var signupmodal = document.querySelector(".signupmodal");
 // Get the button that opens the modal
 var signupbtn = document.getElementById("signup");
-var loginbtn = document.getElementsByName("login");
-console.log(signupbtn);
+var loginbtn = document.querySelector(".login");
+console.log(loginbtn);
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
-// loginbtn.onclick = function() {
-//   loginmodal.style.display = "block";
-// }
-// loginbtn.onclick = function() {
-//   loginmodal.style.display = "block";
-// }
-// signupbtn.onclick = function() {
-//   signupmodal.style.display = "block";
-// }
+loginbtn.onclick = function() {
+    console.log("clicked");
+  loginmodal.style.display = "block";
+}
+signupbtn.onclick = function() {
+  signupmodal.style.display = "block";
+}
 // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//   loginmodal.style.display = "none";
-//   signupmodal.style.display = "none";
-// }
+span.onclick = function() {
+  loginmodal.style.display = "none";
+  signupmodal.style.display = "none";
+}
 
 // When the user clicks anywhere outside of the modal, close it
 // window.onclick = function(event) {
